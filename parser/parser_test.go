4fb988966ed243bc8ec943ba5de00f86b8f188d8
Chris Paterson <cjpaterson.3@gmail.com>
2019-01-27 12:17:28 -0500

    Parse return statement start

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,10 +1,47 @@
 package parser
 
 import (
+	"fmt"
 	"monkey/ast"
 	"monkey/lexer"
 	"testing"
 )
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. Got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+
+		fmt.Printf("%+v\n", returnStmt)
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got=%q", returnStmt.TokenLiteral())
+		}
+	}
+}
 
 func TestLetStatements(t *testing.T) {
 	input := `
@@ -37,6 +74,7 @@ let foobar = 838383;
 
 	for i, tt := range tests {
 		stmt := program.Statements[i]
+		fmt.Printf("%+v\n", stmt)
 		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
 			return
 		}
